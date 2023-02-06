c0ef18e5977572cbf8729f26fa76f7604ffc8435
y00ss <youssef.dev@pm.me>
2023-02-06 17:58:46 +0100

    Initial commit

---
+++ src/main/java/com/spannerdb/clubsupport/ClubSupportApplication.java
@@ -0,0 +1,13 @@
+package com.spannerdb.clubsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spannerdb/clubsupport/ClubSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spannerdb.clubsupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
